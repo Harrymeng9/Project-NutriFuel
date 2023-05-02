59c421e20a435babf779c23564838f8555b8b628
Oscar Sandoval <oscarjs707@gmail.com>
2023-05-01 20:36:15 -0700

    added login files

---
+++ client/src/login/components/app.jsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+
+const App = () => {
+
+  return (
+    <div>
+      <h1>This is the login page</h1>;
+    </div>
+  )
+}
+
+export default App;
