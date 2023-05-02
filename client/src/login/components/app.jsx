f2172fd7ce455407fc180012abaec6b0d7639c36
Oscar Sandoval <oscarjs707@gmail.com>
2023-05-02 16:18:20 -0700

    Added css file support to the webpack bundler

--- client/src/login/components/app.jsx
+++ client/src/login/components/app.jsx
@@ -2,11 +2,12 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 
 
+
 const App = () => {
 
   return (
     <div>
-      <h1>This is the login page</h1>;
+      <h1>This is the login page</h1>
     </div>
   )
 }
