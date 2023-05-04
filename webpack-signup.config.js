71690011d907dc7d13a8d954aa1d5000c82e4e4c
Oscar Sandoval <oscarjs707@gmail.com>
2023-05-03 19:28:49 -0700

    Webpack signup file

---
+++ webpack-signup.config.js
@@ -0,0 +1,27 @@
+var path = require("path");
+var SRC_DIR = path.join(__dirname, "/client/src");
+var DIST_DIR = path.join(__dirname, "/client/dist");
+
+module.exports = {
+  entry: `${SRC_DIR}/login&signup/signup/index.jsx`,
+  output: {
+    filename: "bundle.js",
+    path: `${DIST_DIR}/signup`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:jsx|mjs|cjs|js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+
+    ],
+  }
+};
