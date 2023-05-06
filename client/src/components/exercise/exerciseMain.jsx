8ec62c38d3afa6922c237de082fd0ddd39f3a8e3
achaidez <chaidez62@gmail.com>
2023-05-05 20:38:17 -0600

    Databse pool comnnection and exercise queries. Also some server posts and get's for my section

--- client/src/components/exercise/exerciseMain.jsx
+++ client/src/components/exercise/exerciseMain.jsx
@@ -1,12 +1,21 @@
 import ReactDOM from 'react-dom';
 import React, { useState, useEffect } from 'react';
 import AddExercise from './addExercise.jsx';
+import axios from 'axios';
+import ExerciseLogCard from './exerciseLogCard.jsx';
 
 const ExerciseMain = () => {
-  const [exerciseLog, setExerciseLog] = UseState();
+  const [exerciseLog, setExerciseLog] = useState([]);
 
   useEffect(() => {
-
+    axios.get('/exerciseLog', { params: { user_id: 1 } })
+    .then(data => {
+      console.log('useEffect data', data);
+      setExerciseLog(data.data);
+    })
+    .catch(err => {
+      console.log('exerciseLog useEffect err', err);
+    })
   }, []);
 
   function addExercise () {
@@ -17,7 +26,14 @@ const ExerciseMain = () => {
     <div>
       <h1>Exercise Log</h1>
       <div>
-        exercise log here
+      {exerciseLog.map(entry => {
+          console.log(entry)
+          return (
+            <div className="exerciseLogCard" key={entry.name}>
+              <ExerciseLogCard exercise={entry}/>
+            </div>
+          );
+        })}
       </div>
       <button onClick={addExercise}>Add Exercise</button>
     </div>
