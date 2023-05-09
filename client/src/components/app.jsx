c7bb17142deb34c9b15a81f672496d20f9ddab5c
XQ2013 <xiqiaolearner@gmail.com>
2023-05-09 01:17:22 -0500

    add profile react router

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,8 +1,13 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+
 import ExerciseMain from './exercise/exerciseMain.jsx';
 import Nutrition from './nutrition/Nutrition.jsx';
 import FriendNChat from './friendlist&&chat/friend&chat.jsx';
+import Profile from './profile/profile.jsx';
 
 const App = () => {
 
@@ -13,7 +18,21 @@ const App = () => {
       <Nutrition />
       <div>**********************************************</div>
       <FriendNChat />
+
+      <Profile />
+
+      <div>
+        <Link to="/profile">Profile
+          <FontAwesomeIcon icon={faUser} />
+        </Link>
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+
     </div>
+
+
   )
 }
 
--- client/src/components/profile/profile.jsx
+++ client/src/components/profile/profile.jsx
@@ -1,11 +1,88 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import ProfileEdit from './profileEdit.jsx';
 
-const ExerciseMain = () => {
+
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
+
+
+const axios = require('axios');
+
+
+const Profile = () => {
+
+  const [profileData, setProfileData] = useState({});
+  const [friendsCount, setFriendsCount] = useState(0);
+
+
+  useEffect(() => {
+    axios.get('/profile', { params: { "user_id": 1 } })
+      .then((data) => {
+        setProfileData(data.data);
+        setFriendsCount(data.data.friends.length)
+      })
+      .catch((err) => {
+        console.log('err', err);
+      })
+  }
+    , []);
+
+
+
 
   return (
+
+
+
     <div>
-      <h1>Profile Section</h1>;
+      <h2>Profile Section</h2>
+      <Avatar
+        alt={profileData.username}
+        src={profileData.photo}
+        sx={{
+          width: 100,
+          height: 100,
+          border: '1px solid',
+          fontWeight: 'bold',
+        }}
+      />
+
+      <h3>{profileData.username}</h3>
+      <div>{ friendsCount} Friends</div>
+      <></>
+      
+
+      <div>
+        <Link to="/profile/edit">
+          <Button variant="outlined">Edit</Button>
+        </Link>
+        <Routes>
+          <Route
+            path="/profile/edit"
+            element={<ProfileEdit />} />
+        </Routes>
+      </div>
+
+      <List>
+        <ListItem>
+          <ListItemText primary ="email" secondary={profileData.email} />
+        </ListItem>
+      </List>
+
+
+
+
+
+
+
     </div>
   )
 }
