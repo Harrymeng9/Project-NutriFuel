e5f8f624d9ccd6f7ad9cd6fc10c48902af229606
XQ2013 <xiqiaolearner@gmail.com>
2023-05-10 02:10:03 -0500

    Working on react router

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,5 +1,6 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
+import { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 import { Routes, Route, Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
@@ -8,25 +9,39 @@ import ExerciseMain from './exercise/exerciseMain.jsx';
 import Nutrition from './nutrition/Nutrition.jsx';
 import FriendNChat from './friendlist&&chat/friend&chat.jsx';
 import Profile from './profile/profile.jsx';
+import ProfileEdit from './profile/profileEdit.jsx';
 
 const App = () => {
+
+  const [path, setPath] = useState(window.location.pathname);
+  useEffect(() => {
+    setPath(window.location.pathname)
+  }, []);
+
 
   return (
     <div>
       <h1>This is my React app!</h1>
-      <ExerciseMain />
-      <Nutrition />
+      {/* <ExerciseMain /> */}
+      {/* <Nutrition /> */}
       <div>**********************************************</div>
-      <FriendNChat />
+      {/* <FriendNChat /> */}
 
       <Profile />
 
+      {/* <div>{path}</div> */}
       <div>
-        <Link to="/profile">Profile
-          <FontAwesomeIcon icon={faUser} />
-        </Link>
+        <div className='ProfileIcon' onClick={() => { setPath(window.location.pathname) }}>
+          <Link to="/profile">
+            <FontAwesomeIcon icon={faUser} size="2x"/>
+            <div>Profile</div>
+          </Link>
+        </div>
         <Routes>
           <Route path="/profile" element={<Profile />} />
+          <Route
+            path="/profileedit"
+            element={<ProfileEdit />} />
         </Routes>
       </div>
 
@@ -35,5 +50,6 @@ const App = () => {
 
   )
 }
+
 
 export default App;
