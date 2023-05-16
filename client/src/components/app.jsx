3cc151e5e35a4a350b411e7824bba7ed27262b2a
Oscar Sandoval <oscarjs707@gmail.com>
2023-05-15 19:06:18 -0700

    Refresh page bugfix

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -84,10 +84,12 @@ const App = () => {
     onAuthStateChanged(auth, user => {
       console.log(user);
       if (user) {
-        user.getIdToken()
-          .then((token) => {
-            console.log(token);
-          });
+        userInfo.current.email = user.email;
+        userInfo.current.uid = user.uid;
+        // user.getIdToken()
+        //   .then((token) => {
+        //     console.log(token);
+        //   });
       } else {
         navigate('/login');
       }
