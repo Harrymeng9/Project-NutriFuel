7e3cad7e247723af362c6186c82ed500979e54dd
Alex Chaidez <102176344+achaidez@users.noreply.github.com>
2023-05-20 11:23:08 -0600

    Update exerciseLogCard.jsx

--- client/src/components/exercise/exerciseLogCard.jsx
+++ client/src/components/exercise/exerciseLogCard.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './exerciseCss.css';
+import axios from 'axios';
+import { useNavigate, Routes, Route, Link } from 'react-router-dom';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 const ExerciseLogCard = (props) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   const theme = createTheme({
     palette: {
       primary: {
@@ -26,19 +31,25 @@ const ExerciseLogCard = (props) => {
       }
     })
   }
+
   if (props.exercise.exercise_name !== undefined) {
     return (
       <div>
-        Name - {props.exercise.exercise_name}<br></br>
-        Time - {props.exercise.time} mins
-        <div></div>
-        <br></br>
+      <div>
+        <Typography variant='h8'>
+          Name - {props.exercise.exercise_name}<br></br>
+          Time - {props.exercise.time} mins<br></br>
+        </Typography>
+        <Button variant='outlined' onClick={deleteExercise}>Delete Exercise</Button><br></br>
+      </div><br></br>
       </div>
     )
   } else if (props.exercise.calories !== undefined) {
     return (
       <div>
-        Calories Burned = {props.exercise.calories}
+        <Typography variant='h8'>
+          Calories Burned = {props.exercise.calories}
+        </Typography>
       </div>
     )
   }
